cd4af4003d8b6036c1b52035ca6fdff2fc9f28b5
alex-v-10 <alex.ru9311@gmail.com>
2024-02-19 23:30:59 +0300

    добавляет маршруты

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,12 +1,41 @@
 import MainPage from '../pages/main-page';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { AppRoute } from '../const';
+import NotFoundPage from '../pages/not-found-page';
+import LoginPage from '../pages/login-page';
+import FavoritesPage from '../pages/favorites-page';
+import OfferPage from '../pages/offer-page';
 
 type AppProps = {
   placesCount: number;
 }
 
-function App({placesCount}: AppProps): JSX.Element {
+function App({ placesCount }: AppProps): JSX.Element {
   return (
-    <MainPage placesCount={placesCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<MainPage placesCount={placesCount} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginPage />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<FavoritesPage />}
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<OfferPage />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundPage />}
+        />
+      </Routes>
+    </BrowserRouter >
   );
 }
 
---
+++ src/const.tsx
@@ -0,0 +1,6 @@
+export const AppRoute = {
+  Root: '/',
+  Login: '/login',
+  Favorites: '/favorites',
+  Offer: '/offer'
+} as const;
