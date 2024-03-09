915f22b87e3fecc84406143c1ca43a382426484a
alex-v-10 <alex.ru9311@gmail.com>
2024-03-09 09:30:36 +0300

    исправляет город в титуле списка

--- src/components/offers-list.tsx
+++ src/components/offers-list.tsx
@@ -15,7 +15,7 @@ function OffersList({ offers }: OffersListProps): JSX.Element {
       <div className="cities__places-container container">
         <section className="cities__places places">
           <h2 className="visually-hidden">Places</h2>
-          <b className="places__found">{offers.length} places to stay in Amsterdam</b>
+          <b className="places__found">{offers.length} places to stay in {offers[0].city.name}</b>
           <form className="places__sorting" action="#" method="get">
             <span className="places__sorting-caption">Sort by</span>
             <span className="places__sorting-type" tabIndex={0}>
