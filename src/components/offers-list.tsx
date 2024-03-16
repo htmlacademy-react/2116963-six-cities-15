a995758964e9e12820ca0751eb54e31d4888bb3a
alex-v-10 <alex.ru9311@gmail.com>
2024-03-16 08:59:01 +0300

    реализует сортировку

--- src/components/offers-list.tsx
+++ src/components/offers-list.tsx
@@ -1,11 +1,8 @@
-import type { CityName, Offer } from '../types/offer';
-import Card from './card';
-import Map from './map';
-import { useEffect } from 'react';
-import EmptyList from './empty-list';
 import classNames from 'classnames';
-import { useActionCreators } from '../hooks/state';
-import { offersActions } from '../store/slices/offers';
+import type { CityName, Offer } from '../types/offer';
+import EmptyList from './empty-list';
+import Map from './map';
+import SortingList from './sorting-list';
 
 type OffersListProps = {
   offers: Offer[];
@@ -13,46 +10,11 @@ type OffersListProps = {
 }
 
 function OffersList({ offers, cityName }: OffersListProps): JSX.Element {
-  const { setActiveId } = useActionCreators(offersActions);
-
-  useEffect(() => () => {
-    setActiveId('');
-  });
-
   return (
     <div className="cities">
       <div className={classNames('cities__places-container container', { 'cities__places-container--empty': !offers.length })}>
         {offers.length ?
-          <section className="cities__places places">
-            <h2 className="visually-hidden">Places</h2>
-            <b className="places__found">{offers.length} place{offers.length > 1 && 's'} to stay in {cityName}</b>
-            <form className="places__sorting" action="#" method="get">
-              <span className="places__sorting-caption">Sort by</span>
-              <span className="places__sorting-type" tabIndex={0}>
-                Popular
-                <svg className="places__sorting-arrow" width={7} height={4}>
-                  <use xlinkHref="#icon-arrow-select" />
-                </svg>
-              </span>
-              <ul className="places__options places__options--custom">
-                <li className="places__option places__option--active" tabIndex={0} >
-                  Popular
-                </li>
-                <li className="places__option" tabIndex={0}>
-                  Price: low to high
-                </li>
-                <li className="places__option" tabIndex={0}>
-                  Price: high to low
-                </li>
-                <li className="places__option" tabIndex={0}>
-                  Top rated first
-                </li>
-              </ul>
-            </form>
-            <div className="cities__places-list places__list tabs__content">
-              {offers.map((offer: Offer) => <Card classStart='cities' offer={offer} setActiveId={setActiveId} key={offer.id} />)}
-            </div>
-          </section> :
+          <SortingList offers={offers} cityName={cityName} /> :
           <section className="cities__no-places">
             <EmptyList classStart='cities' cityName={cityName} />
           </section>}
---
+++ src/components/sorting.tsx
@@ -0,0 +1,69 @@
+import classNames from 'classnames';
+import { useEffect, useState } from 'react';
+import { SORTING_OPTIONS } from '../const';
+import { SortingOption } from '../types/sorting';
+
+type SortingProps = {
+  currentOption: SortingOption;
+  setCurrentOption: React.Dispatch<React.SetStateAction<SortingOption>>;
+}
+
+function Sorting({ currentOption, setCurrentOption }: SortingProps) {
+  const [isOpened, setIsOpened] = useState(false);
+
+  useEffect(() => {
+    if (isOpened) {
+      const handleEsc = (evt: KeyboardEvent) => {
+        if (evt.key === 'Escape' || evt.key === 'Esc') {
+          setIsOpened(false);
+        }
+      };
+
+      const handleClickOutside = () => {
+        setIsOpened(false);
+      };
+
+      document.addEventListener('keydown', handleEsc);
+      document.addEventListener('click', handleClickOutside);
+
+      return () => {
+        document.removeEventListener('keydown', handleEsc);
+        document.removeEventListener('click', handleClickOutside);
+      };
+    }
+  }, [isOpened]);
+
+  return (
+    <form className="places__sorting" action="#" method="get" >
+      <span className="places__sorting-caption">Sort by</span>
+      <span className="places__sorting-type" tabIndex={0}
+        onClick={(evt) => {
+          evt.stopPropagation();
+          setIsOpened((prevState) => !prevState);
+        }}
+      >
+        {currentOption.text}
+        <svg className="places__sorting-arrow" width={7} height={4}>
+          <use xlinkHref="#icon-arrow-select" />
+        </svg>
+      </span>
+      <ul className={classNames('places__options places__options--custom', { 'places__options--opened': isOpened })}>
+        {SORTING_OPTIONS.map((option) => (
+          <li className={classNames('places__option', { 'places__option--active': option === currentOption })}
+            tabIndex={0}
+            onClick={() => {
+              if (option !== currentOption) {
+                setCurrentOption(option);
+              }
+            }}
+            key={option.text}
+          >
+            {option.text}
+          </li>
+        ))}
+      </ul>
+    </form >
+  );
+}
+
+export default Sorting;
